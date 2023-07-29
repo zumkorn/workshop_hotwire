e6dd507daeb773ebf26917b7cb16cdb784ed0c45
Sergey Korolev <skorolev@sent.com>
2023-07-29 21:14:28 +0700

    Init commit

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -9,7 +9,11 @@ document.addEventListener("turbo:before-render", (event) => {
   prevPath = window.location.pathname;
   event.detail.render = async (prevEl, newEl) => {
     await new Promise((resolve) => setTimeout(() => resolve(), 0));
-    morphdom(prevEl, newEl);
+    morphdom(prevEl, newEl, {
+      onBeforeElUpdated: function(prevEl, _newEl) {
+        return !(prevEl.dataset.morphPermanent);
+      },
+    })
   };
 
   if (document.startViewTransition) {
